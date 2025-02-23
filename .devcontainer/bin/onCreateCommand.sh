dbfca122281b637fecb2c4a634d725a3f797989f
wmcdonald404 <wmcdonald@gmail.com>
2025-02-23 21:23:23 +0000

    Drop in a config stub to generate aliases for AWS profiles. Hook that into OMB custom.

--- .devcontainer/bin/onCreateCommand.sh
+++ .devcontainer/bin/onCreateCommand.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
-dnf -y install git gh glab awscli2
+
+# Baseline AWS tooling and quality-of-life bits
+dnf -y install awscli2 bash-completion gh git glab 
 dnf -y update 
 
+# Install Oh-My-Bash and disable the default theme
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
 sed -ie 's/^OSH_THEME="font"/# OSH_THEME="font"/' ~/.bashrc
+
+# auto-create aliases to export AWS_PROFILE for each configured AWS CLI profile
+cat > /etc/profile.d/awsprofiles.sh <<EOF
+for profile in \$(aws configure list-profiles | grep -v default)
+do
+  alias \$profile="export AWS_PROFILE=\$profile"
+done
+EOF
+
+ln -s /etc/profile.d/awsprofiles.sh /root/.oh-my-bash/custom/

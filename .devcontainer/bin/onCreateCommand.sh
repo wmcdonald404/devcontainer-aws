51384e657b0c5dd35d9b5f7e65af40caaf1affbf
wmcdonald404 <wmcdonald@gmail.com>
2025-02-24 22:45:13 +0000

    Add .gitignore for ./temp. Refine decontainer def and onCreate script.

--- .devcontainer/bin/onCreateCommand.sh
+++ .devcontainer/bin/onCreateCommand.sh
@@ -8,12 +8,13 @@ dnf -y update
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
 sed -ie 's/^OSH_THEME="font"/# OSH_THEME="font"/' ~/.bashrc
 
+# Drop in AWS-specific environment config
+cat > /etc/profile.d/awsenv.sh <<EOF
 # auto-create aliases to export AWS_PROFILE for each configured AWS CLI profile
-cat > /etc/profile.d/awsprofiles.sh <<EOF
 for profile in \$(aws configure list-profiles | grep -v default)
 do
   alias \$profile="export AWS_PROFILE=\$profile"
 done
-EOF
 
-ln -s /etc/profile.d/awsprofiles.sh /root/.oh-my-bash/custom/
+EOF
+ln -s /etc/profile.d/awsenv.sh ${HOME}/.oh-my-bash/custom/
